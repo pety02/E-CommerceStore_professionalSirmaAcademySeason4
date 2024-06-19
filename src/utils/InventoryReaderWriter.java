0aac205fb922edc55636a90f43319e91a80abcf4
pety02 <pety02@abv.bg>
2024-06-19 20:31:13 +0300

    Some more comments generated.

--- src/utils/InventoryReaderWriter.java
+++ src/utils/InventoryReaderWriter.java
@@ -2,22 +2,40 @@ package utils;
 
 import interfaces.Readable;
 import interfaces.Writeable;
-import model.AbstractItem;
 import model.InventoryItem;
 
 import java.io.*;
 
+/**
+ *
+ */
 public class InventoryReaderWriter implements Writeable, Readable {
 
+    /**
+     *
+     */
     public InventoryReaderWriter() {
 
     }
 
+    /**
+     *
+     * @param filename
+     * @return
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
     @Override
     public InventoryItem read(String filename) throws IOException, ClassNotFoundException {
         return (InventoryItem) new ObjectInputStream(new FileInputStream(filename)).readObject();
     }
 
+    /**
+     *
+     * @param item
+     * @param filename
+     * @throws IOException
+     */
     @Override
     public void write(InventoryItem item, String filename) throws IOException {
         new ObjectOutputStream(new FileOutputStream(filename)).writeObject(item);
